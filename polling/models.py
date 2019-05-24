90a67db7596bf4e520c798a1ae0fe21bc957f29a
Tim Pauley <timothyjpauley@gmail.com>
2019-05-24 14:12:04 -0700

    updating polling

--- polling/models.py
+++ polling/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):
+    title  = models.CharField(max_length=128)
+    text = models.TextField(blank=True)
+    score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.titlefrom django.db import models
+
+# Create your models here.
