670a7d01b874ebd9d32bd1e890acdc2daa87cdb5
Tim Pauley <timothyjpauley@gmail.com>
2019-06-01 15:42:29 -0700

    adding updates to assignment 08

--- blogging/tests.py
+++ blogging/tests.py
@@ -1,26 +1,69 @@
-from django.db import models
+import datetime
+from django.utils.timezone import utc
+from django.test import TestCase
 from django.contrib.auth.models import User
+from blogging.models import Post, Category
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=128)
-    text = models.TextField(blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
-    published_date = models.DateTimeField(blank=True, null=True)
+class PostTestCase(TestCase):
+    fixtures = ["blogging_test_fixture.json",]
 
-    def __str__(self):
-        return self.title
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+
+    def test_string_representation(self):
+        expected = "This is a title"
+        p1 = Post(title=expected)
+        actual = str(p1)
+        self.assertEqual(expected, actual)
+
+class CategoryTestCase(TestCase):
+    
+    def test_string_representation(self):
+        expected = "A Category"
+        c1 = Category(name=expected)
+        actual = str(c1)
+        self.assertEqual(expected, actual)
+
+class FrontEndTestCase(TestCase):
+    """test views provided in the front-end"""
+    fixtures = ["blogging_test_fixture.json", ]
+
+    def setUp(self):
+        self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
+        self.timedelta = datetime.timedelta(15)
+        author = User.objects.get(pk=1)
+        for count in range(1, 11):
+            post = Post(title="Post %d Title" % count,
+                        text="foo",
+                        author=author)
+            if count < 6:
+                # publish the first five posts
+                pubdate = self.now - self.timedelta * count
+                post.published_date = pubdate
+            post.save()
+
+    def test_list_only_published(self):
+        resp = self.client.get("/")
+        # the content of the rendered response is always a bytestring
+        resp_text = resp.content.decode(resp.charset)
+        self.assertTrue("Recent Posts" in resp_text)
+        for count in range(1, 11):
+            title = "Post %d Title" % count
+            if count < 6:
+                self.assertContains(resp, title, count=1)
+            else:
+                self.assertNotContains(resp, title)
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=128) 
-    description = models.TextField(blank=True)
-    posts = models.ManyToManyField(Post, blank=True, related_name="categories")
-
-    def __str__(self):
-        return self.name 
-
-    class Meta:
-        verbose_name_plural = "Categories"
+    def test_details_only_published(self):
+        for count in range(1, 11):
+            title = "Post %d Title" % count
+            post = Post.objects.get(title=title)
+            resp = self.client.get("/posts/%d/" % post.pk)
+            
+            if count < 6:
+                self.assertEqual(resp.status_code, 200)
+                self.assertContains(resp, title)
+            else:
+                self.assertEqual(resp.status_code, 404)
